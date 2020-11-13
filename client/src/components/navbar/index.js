48a09e73b5de82e78a6eceae8e98c1d224f36884
benklinton <benklinton@gmail.com>
2020-11-13 11:24:42 -0700

    changed navbar items/ links

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -24,13 +24,13 @@ function Navigation() {
               </Nav.Item>
               <Router>
                 <Nav.Item>
-                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/projects">Projects</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                   <Nav.Link href="/contact">Contact</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                  <Nav.Link href="/resume">Resume</Nav.Link>
                 </Nav.Item>
               </Router>
             </ul>
