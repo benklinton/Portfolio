68a84733e6e4224ebf04ffb84e4490e2e4801290
benklinton <benklinton@gmail.com>
2020-11-20 20:03:02 -0700

    fixed style for projects component

--- client/src/components/projects/index.js
+++ client/src/components/projects/index.js
@@ -4,27 +4,29 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 function Projects() {
     return (
         <section className='page-section portfolio'>
             <Container>
-                <h2 id='projects' className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+                <h2 id='projects' className="page-section-heading text-center text-uppercase text-secondary mb-0">Projects</h2>
                 <div class="divider-custom">
                     <div class="divider-custom-line"></div>
                 </div>
-                <Row>
+                <Row className="row-margin">
                     <CardDeck>
                         <Card>
                             <div class="flip-card">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="https://via.placeholder.com/200x200" alt="project"></img>
+                                        <img src="./assets/Chip-Shop-Logo.png" alt="Chip Shop Logo" className="chip-shop"></img>
                                     </div>
                                     <div class="flip-card-back">
-                                        <h1>John Doe</h1>
-                                        <p>Architect & Engineer</p>
-                                        <p>We love that guy</p>
+                                        <h1>Chip Shop</h1>
+                                        <p>A simple one page website with info about the small business Chip Shop </p>
+                                        <Button target="_blank" variant="outline-info" href="http://www.chipshoprepair.com" className='margin-button'>Website</Button>
+                                        <Button target="_blank" variant="outline-info" href="https://github.com/benklinton/Chip-Shop" className='margin-button'>Github</Button>
                                     </div>
                                 </div>
                             </div>
@@ -33,12 +35,13 @@ function Projects() {
                             <div class="flip-card">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="https://via.placeholder.com/200x200" alt="project"></img>
+                                        <img src="./assets/Soffe-Doodles-Logo.png" alt="Soffe-Doodles Logo"></img>
                                     </div>
                                     <div class="flip-card-back">
-                                        <h1>John Doe</h1>
-                                        <p>Architect & Engineer</p>
-                                        <p>We love that guy</p>
+                                        <h1>Soffe Doodles</h1>
+                                        <p>A multi page website for a local dog breeders business</p>
+                                        <Button target="_blank" variant="outline-info" href="http://soffedoodles.net" className='margin-button'>Website</Button>
+                                        <Button target="_blank" variant="outline-info" href="https://github.com/benklinton/Soffe-Doodles" className='margin-button'>Github</Button>
                                     </div>
                                 </div>
                             </div>
@@ -47,12 +50,13 @@ function Projects() {
                             <div class="flip-card">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="https://via.placeholder.com/200x200" alt="project"></img>
+                                        <img src="https://via.placeholder.com/200x200?text=Coming+Soon" alt="project"></img>
                                     </div>
                                     <div class="flip-card-back">
-                                        <h1>John Doe</h1>
-                                        <p>Architect & Engineer</p>
-                                        <p>We love that guy</p>
+                                        <h1>SoopsArt</h1>
+                                        <p>A business website for the display and sell of the works of Sophie Gifford </p>
+                                        <p>Currently in Development</p>
+                                        <Button target="_blank" variant="outline-info" href="https://github.com/benklinton/SoopsArt" className='margin-button'>Github</Button>
                                     </div>
                                 </div>
                             </div>
